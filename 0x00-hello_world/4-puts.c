691075541e6c9dd1a7188c451f2005488d354b5c
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-02-17 17:11:15 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 /**
- * *Description: main - A program that prints a line using the printf function
+ * *Description: main - A program that prints a line using the puts function
  * *Return: 0 if Success
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
