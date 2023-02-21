06d6f3e4bad8bcf50c1c63405f7cffb275d1a88d
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-02-21 02:52:49 -0800

    0. _putchar

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  main - A program that prints the size if various computer types
- *  Return: Always 0 (Success)
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
@@ -13,4 +13,3 @@ printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 return (0);
 }
-
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,7 @@
+/**
+ * main - a program to print _putchar without stdio
+ *
+ * Return: Always 0.
+ */
+
+write(0, "_putchar\n", 6)
