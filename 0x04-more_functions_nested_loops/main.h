527188a04874d7c55162667322410beed9218418
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-02-24 12:55:50 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 int _isdigit(int c);
 int _isupper(int c);
 int mul(int a, int b);
-oid print_numbers(void);
+void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
