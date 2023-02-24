1aff870c0f03d7be42aa41853e9b0ea33d773ae8
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-02-24 10:07:51 -0800

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,9 @@
 int _isdigit(int c);
 int _isupper(int c);
+int mul(int a, int b);
+oid print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
