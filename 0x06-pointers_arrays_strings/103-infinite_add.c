cd3df2a4728b9d214ce157bb5ad09d14af829a70
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-03-02 15:30:10 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,66 +1,48 @@
+#include "main.h"
+#include <stdio.h>
 /**
- * infinite_add - Add up two numbers stored in given char arrays
- * @n1: The first number
- * @n2: The second number
- * @r: Pointer to the buffer to store result
- * @size_r: The size of the buffer
- *
- * Return: 0 if buffer too small to store result, else return pointer to buffer
+ * infinite_add - adds values of two strings together
+ * @n1: first string of integers
+ * @n2: second string of ints
+ * @r: result string
+ * @size_r: size of result string
+ * Return: void
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
-	char tmp[10000];
+	int len1 = 0, num1 = 0, sum1 = 0;
+	int len2 = 0, num2 = 0, sum2 = 0;
+	int num = 0, sum = 0, i, j;
+	char temp, n1c[1000], n2c[1000];
 
-	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
-	while (n1[l1] != '\0')
-		l1++;
-	while (n2[l2] != '\0')
-		l2++;
-	if (l1 + 2 > size_r || l2 + 2 > size_r)
+	while (n1[len1] != '\0')
+		len1++;
+	while (n2[len2] != '\0')
+		len2++;
+	if (len1 >= size_r || len2 >= size_r)
 		return (0);
-	l1--;
-	l2--;
-	while (i <= l1 || i <= l2)
+	for (j = 0, i = len1 - 1; j < len1; j++, i--)
+		n1c[j] = n1[i];
+	n1c[j] = '\0';
+	for (j = 0, i = len2 - 1; j < len2; j++, i--)
+		n2c[j] = n2[i];
+	n2c[j] = '\0';
+	while (num < size_r - 1)
 	{
-		num1 = num2 = 0;
-		if (i <= l1)
-			num1 = n1[l1 - i] - '0';
-		if (i <= l2 && (l2 - i) >= 0)
-			num2 = n2[l2 - i] - '0';
-		sum = num1 + num2 + carry;
-		if (sum >= 10)
-		{
-			carry = 1;
-			sum -= 10;
-		}
-		else
-			carry = 0;
-		r[i] = sum + '0';
-		i++;
-		rl++;
-	}
-	if (carry > 0)
-	{
-		r[i] = carry + '0';
-		r[i + 1] = '\0';
-	}
-	i = tmpl = 0;
-	while (i <= rl)
-	{
-		tmp[i] = r[rl - i];
-		tmpl++;
-		i++;
-	}
-	i = 0;
-	while (i < tmpl)
-	{
-		if (r[i] == '\0')
-		{
+		if (num1 >= len1 && num2 >= len2 && sum == 0)
 			break;
-		}
-		r[i] = tmp[i];
-		i++;
+		sum1 = num1 < len1 ? n1c[num1] - '0' : 0;
+		sum2 = num2 < len2 ? n2c[num2] - '0' : 0;
+		sum += (sum1 + sum2);
+		r[num] = sum % 10 + '0';
+		sum = sum < 10 ? 0 : 1;
+		num++, num1++, num2++;
 	}
+	if (sum != 0)
+		return (0);
+	r[num] = '\0';
+	for (j = 0, i = --num; j < num / 2; j++, i--)
+		temp = r[j], r[j] = r[i], r[i] = temp;
 	return (r);
 }
