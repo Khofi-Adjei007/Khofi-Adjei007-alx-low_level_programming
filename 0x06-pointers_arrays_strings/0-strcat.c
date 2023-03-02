2b48e56585766f5488755b528409c1cff6e6d411
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-03-02 01:01:53 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _memset - fills memory with a constant byte.
+* @s: input array
+* @b: constant
+* @n: n bytes to fill
+* Return: s
+*/
+
+char *_strcat(char *dest, char *src);
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+{
+	s[i] = b;
+}
+	return (s);
+}
+
