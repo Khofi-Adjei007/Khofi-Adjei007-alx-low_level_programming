c7bdcfb3201ae35c814d1096c889f703ebb71381
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-03-10 15:57:34 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,41 +1,29 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
-#include <string.h>
-/**
- * checker - checks for valid input
- * @argc: argument count
- * @i: counter for argv[]
- * @j: counter for argv[][]
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
- */
-int checker(int argc, int i, unsigned int j, char *argv[])
-{
-	for (i = 1; i <= argc; i++)
-		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-			if (isdigit(argv[i][j]) == 0)
-				return (1);
-	return (0);
-}
-/**
- * main - adds all arguments together if they are digits.
- * @argc: argument count only accepts ints separated by spaces.
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
- */
-int main(int argc, char *argv[])
-{
-	int result, i;
+#include <ctype.h>
 
-	result = 0;
-	if (checker(argc, 1, 0, argv) == 1)
+/**
+ * main - function to add variables
+ * @argc: number of arguments
+ * @argv: arguments being passed
+ * Return: Always 0 or 1
+ */
+int main(int argc, char **argv)
+{
+	int a, b, addition;
+
+	for (a = 1; a < argc; a++)
 	{
-		printf("Error\n");
-		return (1);
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (!isdigit(argv[a][b]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		addition = addition + atoi(argv[a]);
 	}
-	for (i = 1; i < argc; i++)
-		result += atoi(argv[i]);
-	printf("%d\n", result);
+	printf("%i\n", addition);
 	return (0);
 }
