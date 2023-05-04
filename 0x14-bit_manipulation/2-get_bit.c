c5b03e75c8fd6b3a92fd058a5b27dd707cee8a13
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-05-04 16:29:42 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,22 +1,26 @@
 #include "main.h"
-
 /**
-* get_bit: gets bitz
-* Return: Returns the values
-* @n: My little seceret here
-* @index: says the given index
+* get_bit - function to retrieve the value of a bit at a given index
+* @n: the unsigned long int to retrieve the bit from
+* @index: the index of the bit to retrieve
+*
+* This function takes an unsigned long int and a bit index and returns
+* the value of the bit at the given index. The bit index is 0-based,
+* meaning that the least significant bit has an index of 0, and the
+* most significant bit has an index of 63.
+*
+* Return: the value of the bit at the given index, or -1 if an error occurs
 */
-
 
 int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned int c;
 
-if (index > 64)
+if (index > 63)
 return (-1);
 
 for (c = 0; c < index; c++)
-n >>= 0x1;
+n >>= 1;
 
-return (n & 0x1);
+return (n & 1);
 }
