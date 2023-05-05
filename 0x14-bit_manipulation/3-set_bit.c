1b0d707f1cfcf66244f7bd09274b92e03fc62348
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-05-05 05:40:41 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,34 +1,12 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
-/**
-* main - entry point for program
-*
-* This program demonstrates the functionality of the `set_bit` function.
-* It sets a bit at a given index in an unsigned long int variable and
-* prints the updated value of the variable.
-*
-* Return: Always 0 indicating successful execution of the program.
-*/
-
-int main(void)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int n;
+    if (index >= sizeof(unsigned long int) * 8)
+        return (-1);
 
-/* Initialize variable n to 1024 and set bit at index 5 */
-n = 1024;
-set_bit(&n, 5);
-printf("%lu\n", n);
-
-/* Initialize variable n to 0 and set bit at index 10 */
-n = 0;
-set_bit(&n, 10);
-printf("%lu\n", n);
-
-/* Initialize variable n to 98 and set bit at index 0 */
-n = 98;
-set_bit(&n, 0);
-printf("%lu\n", n);
-
-return (0);
+    *n |= 1UL << index;
+    return (1);
 }
+
