c264bb203ecc94daf7bee175b698a4ac06857587
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-06-19 09:26:55 -0700

    libdynamic.so, main.h

---
+++ 0x18-dynamic_libraries/dynamic.c
@@ -0,0 +1,140 @@
+#include <stddef.h>
+
+/* Prints a character to the standard output */
+int _putchar(char c)
+{
+/* Implement _putchar function here */
+return (0);
+}
+
+/* Checks if a character is lowercase */
+int _islower(int c)
+{
+/* Implement _islower function here */
+return (0);
+}
+
+/* Checks if a character is alphabetic */
+int _isalpha(int c)
+{
+/* Implement _isalpha function here */
+return (0);
+}
+
+/* Calculates the absolute value of an integer */
+int _abs(int n)
+{
+/* Implement _abs function here */
+return (0);
+}
+
+/* Checks if a character is uppercase */
+int _isupper(int c)
+{
+/* Implement _isupper function here */
+return (0);
+}
+
+/* Checks if a character is a digit */
+int _isdigit(int c)
+{
+/* Implement _isdigit function here */
+return (0);
+}
+
+/* Calculates the length of a string */
+int _strlen(char *s)
+{
+/* Implement _strlen function here */
+return (0);
+}
+
+/* Prints a string to the standard output */
+void _puts(char *s)
+{
+/* Implement _puts function here */
+}
+
+/* Copies a string from source to destination */
+char *_strcpy(char *dest, char *src)
+{
+/* Implement _strcpy function here */
+return (dest);
+}
+
+/* Converts a string to an integer */
+int _atoi(char *s)
+{
+/* Implement _atoi function here */
+return (0);
+}
+
+/* Concatenates two strings */
+char *_strcat(char *dest, char *src)
+{
+/* Implement _strcat function here */
+return (dest);
+}
+
+/* Concatenates n characters from source to destination */
+char *_strncat(char *dest, char *src, int n)
+{
+/* Implement _strncat function here */
+return (dest);
+}
+
+/* Copies n characters from source to destination */
+char *_strncpy(char *dest, char *src, int n)
+{
+/* Implement _strncpy function here */
+return (dest);
+}
+
+/* Compares two strings */
+int _strcmp(char *s1, char *s2)
+{
+/* Implement _strcmp function here */
+return (0);
+}
+
+/* Sets the first n bytes of a memory area to a specific value */
+char *_memset(char *s, char b, unsigned int n)
+{
+/* Implement _memset function here */
+return (s);
+}
+
+/* Copies n bytes from source to destination */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+/* Implement _memcpy function here */
+return (dest);
+}
+
+/* Locates the first occurrence of a character in a string */
+char *_strchr(char *s, char c)
+{
+/* Implement _strchr function here */
+return (NULL);
+}
+
+/* Calculates the length of the initial segment of a string that consists of only characters from another string */
+unsigned int _strspn(char *s, char *accept)
+{
+/* Implement _strspn function here */
+return (0);
+}
+
+/* Searches a string for any of a set of characters */
+char *_strpbrk(char *s, char *accept)
+{
+/* Implement _strpbrk function here */
+return (NULL);
+}
+
+/* Locates the first occurrence of a substring in a string */
+char *_strstr(char *haystack, char *needle)
+{
+/* Implement _strstr function here */
+return (NULL);
+}
