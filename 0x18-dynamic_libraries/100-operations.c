783720e88bad6030589deb50305cced0586223c2
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-06-23 16:10:37 -0700

    100-operations.so

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,19 +1,70 @@
-int add(int a, int b) {
-    return a + b;
+/*
+* Function: add
+* Description: Adds two integers and returns the result.
+* Parameters:
+*     - a: The first integer.
+*     - b: The second integer.
+* Return: The sum of the two integers.
+*/
+
+int add(int a, int b)
+{
+return (a + b);
 }
 
-int sub(int a, int b) {
-    return a - b;
+/*
+* Function: sub
+* Description: Subtracts the second integer
+* from the first integer and returns the result.
+* Parameters:
+*     - a: The first integer.
+*     - b: The second integer.
+* Return: The difference between the first and second integers.
+*/
+
+int sub(int a, int b)
+{
+return (a - b);
 }
 
-int mul(int a, int b) {
-    return a * b;
+/*
+* Function: mul
+* Description: Multiplies two integers and returns the result.
+* Parameters:
+*     - a: The first integer.
+*     - b: The second integer.
+* Return: The product of the two integers.
+*/
+int mul(int a, int b)
+{
+return (a * b);
 }
 
-int div(int a, int b) {
-    return a / b;
+/*
+* Function: div
+* Description: Divides the first integer by the
+* second integer and returns the result.
+* Parameters:
+*     - a: The first integer (dividend).
+*     - b: The second integer (divisor).
+* Return: The quotient of the division.
+*/
+int div(int a, int b)
+{
+return (a / b);
 }
 
-int mod(int a, int b) {
-    return a % b;
+/*
+* Function: mod
+* Description: Computes the remainder of the
+* division of the first integer by the
+* second integer and returns the result.
+* Parameters:
+*     - a: The first integer (dividend).
+*     - b: The second integer (divisor).
+* Return: The remainder of the division.
+*/
+int mod(int a, int b)
+{
+return (a % b);
 }
