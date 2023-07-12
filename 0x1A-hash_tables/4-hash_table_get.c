292638057f1ebcdfaefe66de20bfa8c0e3cc8657
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-07-12 06:56:42 -0700

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,11 +5,13 @@
 
 
 /**
-* hash_table_get - Retrieves a value associated with a key in a hash table.
+* hash_table_get - Retrieves a value
+* associated with a key in a hash table.
 * @ht: The hash table to look into.
 * @key: The key to search for.
 *
-* Return: The value associated with the element, or NULL if key couldn't be found.
+* Return: The value associated with the element,
+* or NULL if key couldn't be found.
 */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
@@ -19,11 +21,11 @@ hash_node_t *temp;
 
 /* Check if the key or hash table is NULL */
 if (key == NULL || ht == NULL)
-return NULL;
+return (NULL);
 
 /* Check if the key is an empty string */
 if (key[0] == '\0')
-return NULL;
+return (NULL);
 
 /* Calculate the index for the given key */
 index = key_index((const unsigned char *)key, ht->size);
@@ -34,10 +36,10 @@ while (temp != NULL)
 {
 /* If the key is found, return the associated value */
 if (strcmp(temp->key, key) == 0)
-return temp->value;
+return (temp->value);
 
 temp = temp->next;
 }
 
-return NULL; /* Key couldn't be found */
+return (NULL); /* Key couldn't be found */
 }
